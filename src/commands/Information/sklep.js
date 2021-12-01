aa21ba8e7d1a7a21e43a3f873a60f4f96fe04f96
Ciochinio <ciochu.michal@gmail.com>
2021-12-01 23:48:36 +0100

    Update sklep.js

--- src/commands/Information/sklep.js
+++ src/commands/Information/sklep.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('Sklep')
+		.setName('sklep')
 		.setDescription('Wykup abonament, który pozwoli Ci grać w WitcherCraft!'),
 	async execute(interaction) {
 		await interaction.reply('https://www.xbox.com/pl-PL/xbox-game-pass/pc-games');
